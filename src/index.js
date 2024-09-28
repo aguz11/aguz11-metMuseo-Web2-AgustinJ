5d7d339ab7e296c7af23f77a190a139123b51eff
aguz11 <agus110196@gmail.com>
2024-09-27 21:10:21 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 import translate from 'node-google-translate-skidz'; 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
