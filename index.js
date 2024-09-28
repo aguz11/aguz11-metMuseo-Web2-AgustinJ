ab00c1955561bdb3f07a3a67d153d9de5d9b65f9
aguz11 <agus110196@gmail.com>
2024-09-27 23:06:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -99,16 +99,17 @@ app.get("/search", async (req, res) => {
     let url = `https://collectionapi.metmuseum.org/public/collection/v1/search?`;
     let hasParameter = false;
 
-    if (departmentId) {
-        url += `departmentId=${departmentId}`;
-        hasParameter = true;
-    }
-
     if (keyword) {
         url += hasParameter ? `&q=${encodeURIComponent(keyword)}` : `q=${encodeURIComponent(keyword)}`;
         hasParameter = true;
     }else if (departmentId) {
-        url += hasParameter ? `&q=` : `q=`;
+        url += hasParameter ? `&q=*` : `q=*`;
+    }
+
+
+    if (departmentId) {
+        url += `departmentId=${departmentId}`;
+        hasParameter = true;
     }
 
     if (geoLocation) {
